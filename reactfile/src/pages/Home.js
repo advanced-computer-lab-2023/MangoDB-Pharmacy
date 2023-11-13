c68c6291791a377a7c463faf98ee522af4ad4af5
lamawagih <lamawagih@gmail.com>
2023-11-13 03:50:29 +0200

    updated homepage

--- reactfile/src/pages/Home.js
+++ reactfile/src/pages/Home.js
@@ -7,16 +7,14 @@ const Home = () => {
     <div className="home">
       <h2>Home Page</h2>
       <p>This is the home page</p>
-      <button onClick={() => history.push("/")}>Home</button>
+      <button onClick={() => history.push("/")}>Login</button>
       <button onClick={() => history.push("/dashboard")}>Dashboard</button>
-      <button onClick={() => history.push("/viewMeds")}>Get meds</button>
       <button onClick={() => history.push("/patientReg")}>
         Register as a patient
       </button>
       <button onClick={() => history.push("/pharmaReg")}>
         Register as a pharmacist
       </button>
-      <button onClick={() => history.push("/addMed")}>Add Med</button>
     </div>
   );
 };
