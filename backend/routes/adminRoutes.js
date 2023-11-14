9c2730ce69d1494f7b0e5c0bbcddaa3242197d01
dxnielsherif <102485130+dxnielsherif@users.noreply.github.com>
2023-11-14 03:13:49 +0200

    Unknown issue

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,35 +1,33 @@
-const express = require('express');
-const adminController = require('../controllers/adminController');
-const pharmacistModel = require('../models/pharmacistModel');
+const express = require("express");
+const adminController = require("../controllers/adminController");
+const pharmacistModel = require("../models/pharmacistModel");
 
 const router = express.Router();
 
-const {protectAdmin} = require('../middleware/adminMiddleware')
+const { protectAdmin } = require("../middleware/adminMiddleware");
 
-router.get('/' , (req,res) => {
-    res.render('../views/adminHome')
-})
+router.get("/", (req, res) => {
+  res.render("../views/adminHome");
+});
 
-router.get('/query',adminController.searchFilter)
-router.get('/viewMed' , adminController.viewMed)
-router.get('/viewPharmacistInfo/:id' , adminController.viewPharmacistInfo)
-router.get('/viewAllPharmacists' , adminController.viewAllPharmacists)
-router.get('/viewAllPatients' , adminController.viewAllPatients)
-router.get('/getPatientsBasicInfo/:id' , adminController.getPatientsBasicInfo)
-router.post('/addAdmin' , adminController.add_admin)
-router.post('/addPharma', adminController.add_pharmacist);
-router.post('/login', adminController.loginAdmin)
-router.post('/pharmacist-approval/:id', adminController.pharmacistApproval)
-router.post('/pharmacist-rejection/:id', adminController.pharmacistRejection)
-router.post('/verify-otp', protectAdmin, adminController.verifyOTP)
-router.post('/reset-password', protectAdmin, adminController.resetPassword)
-router.delete('/deletePharma/:id', adminController.deletePharmacist);
-router.delete('/deletePatient/:id', adminController.deletePatient);
-router.get('/getPendingPharma', adminController.getPendingPharma);
-router.get('/:id', adminController.viewPharmacistInfo);
-
-router.get('/request-otp', protectAdmin, adminController.sendOTP)
-
+router.get("/query", adminController.searchFilter);
+router.get("/viewMed", adminController.viewMed);
+router.get("/viewPharmacistInfo/:id", adminController.viewPharmacistInfo);
+router.get("/viewAllPharmacists", adminController.viewAllPharmacists);
+router.get("/viewAllPatients", adminController.viewAllPatients);
+router.get("/getPatientsBasicInfo/:id", adminController.getPatientsBasicInfo);
+router.post("/addAdmin", adminController.add_admin);
+router.post("/addPharma", adminController.add_pharmacist);
+router.post("/login", adminController.loginAdmin);
+router.post("/pharmacist-approval/:id", adminController.pharmacistApproval);
+router.post("/pharmacist-rejection/:id", adminController.pharmacistRejection);
+router.post("/verify-otp", protectAdmin, adminController.verifyOTP);
+router.post("/reset-password", protectAdmin, adminController.resetPassword);
+router.delete("/deletePharma/:id", adminController.deletePharmacist);
+router.delete("/deletePatient/:id", adminController.deletePatient);
+router.get("/getPendingPharma", adminController.getPendingPharma);
+router.get("/:id", adminController.viewPharmacistInfo);
+router.get("/request-otp", protectAdmin, adminController.sendOTP);
 
 // router.get('/', (req, res) => {
 //     res.status(200).render('Admin/Home');
--- backend/routes/guestRoutes.js
+++ backend/routes/guestRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const guestController = require("../controllers/guestController");
 const router = express.Router();
+const upload = require("../middleware/upload");
 
 const { protect } = require("../middleware/authMiddleware");
 
@@ -10,6 +11,6 @@ router.get("/regPatient", guestController.regPatientView);
 router.post("/regPatient", guestController.regPatient);
 
 router.get("/regPharma", guestController.regPharmaView);
-router.post("/regPharma", guestController.regPharma);
+router.post("/regPharma", upload.array("documents"), guestController.regPharma);
 
 module.exports = router;
