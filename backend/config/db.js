bd0961a2b57505323ef57869b25d95fb5ae13dc9
OmarElZaher <omarelzaher93@gmail.com>
2023-12-17 02:13:40 +0200

    changed db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDB = async() => {
-    try {
-        const conn = await mongoose.connect('mongodb+srv://lamawagih:Lxdxjx2002@cluster0.ox2duku.mongodb.net/')
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(
+			"mongodb+srv://omarelzaher:123@mangodb.v1p5zw3.mongodb.net/Main"
+		);
 
-        console.log(`MongoDB Connected Successfuly: ${conn.connection.host}`.magenta.bold)
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
-}
+		console.log(
+			`MongoDB Connected Successfuly: ${conn.connection.host}`.magenta.bold
+		);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
