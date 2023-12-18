df906049a884e63a672d8d4332e4423cb9ff43b3
lamawagih <lamawagih@gmail.com>
2023-12-18 06:34:27 +0200

    view sales

--- reactfile/src/pages/sales.js
+++ reactfile/src/pages/sales.js
@@ -1,13 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Grid, Typography, Paper, Button, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, Select, MenuItem } from '@mui/material';
-import { pharmacistListItems } from '../components/ListItemsPharma';
+import { Grid, Typography, Paper, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import AdminHeader from "../components/AdminHeader";
 import { getDifMeds, getAllSales, getSalesByMonth, getSalesByMedicine } from '../services/api';
-import { useParams } from 'react-router-dom';
-import InputLabel from '@mui/material/InputLabel';
-import Input from '@mui/material/Input';
-import PharmacistHeader from '../components/PharmacistHeader';
-
 
 const ViewSales = () => {
   const [sales, setSales] = useState([]);
@@ -16,37 +10,37 @@ const ViewSales = () => {
   const [filterType, setFilterType] = useState("all");
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
   const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 1);
+  const [selectedMonthYear, setSelectedMonthYear] = useState(
+    `${selectedYear}-${selectedMonth.toString().padStart(2, '0')}`
+  );
   const [selectedMedicine, setSelectedMedicine] = useState("");
   const [medicineNames, setMedicineNames] = useState([]);
-
-  const handleFilterTypeChange = (event) => {
-    setFilterType(event.target.value);
-  };
 
   const calculateTotalPrice = () => {
     let total = 0;
     sales.forEach((sale) => {
-       total += sale.totalPrice;
+      total += sale.totalPrice;
     });
     return total;
-   };
+  };
 
   const handleYearChange = (event) => {
     setSelectedYear(event.target.value);
   };
 
   const handleMonthChange = (event) => {
-    const yearMonth = event.target.value.split("-");
-    setSelectedYear(parseInt(yearMonth[0]));
-    setSelectedMonth(parseInt(yearMonth[1]));
+    setSelectedMonth(event.target.value);
   };
-  
+
+  const handleMonthYearChange = (event) => {
+    setSelectedMonthYear(event.target.value);
+  };
 
   const handleMedicineChange = (event) => {
     setSelectedMedicine(event.target.value);
   };
 
-  const handleShowSales = async () => {
+  const fetchData = async () => {
     try {
       setIsPending(true);
       setError("");
@@ -56,14 +50,18 @@ const ViewSales = () => {
           const allSalesResponse = await getAllSales();
           setSales(allSalesResponse.sales);
           break;
-          case "month":
-            const yearMonthString = `${selectedYear}-${selectedMonth.toString().padStart(2, '0')}`;
-            const salesByMonthResponse = await getSalesByMonth(yearMonthString);
-            setSales(salesByMonthResponse.sales);
-            break;
+        case "month":
+          if(selectedMonthYear){
+          const salesByMonthResponse = await getSalesByMonth(selectedMonthYear);
+          setSales(salesByMonthResponse.sales);}
+          break;
         case "medicine":
-          const salesByMedicineResponse = await getSalesByMedicine(selectedMedicine);
-          setSales(salesByMedicineResponse.sales);
+          // Fetch data only when a medicine is selected
+          if (selectedMedicine) {
+            const salesByMedicineResponse = await getSalesByMedicine(selectedMedicine);
+            setSales(salesByMedicineResponse.sales);
+          }
+          // If no specific medicine is selected, do not clear existing sales data
           break;
         default:
           break;
@@ -77,6 +75,7 @@ const ViewSales = () => {
   };
 
   useEffect(() => {
+    fetchData();
     getDifMeds()
       .then((response) => {
         setMedicineNames(response.data.medicineNames);
@@ -86,96 +85,102 @@ const ViewSales = () => {
       });
   }, []);
 
-  return (
-    <PharmacistHeader>
-    <Grid container>
-      <Grid item xs={12} sm={3} md={2} lg={2} xl={2} style={{ background: "#f0f0f0", minHeight: "100vh", paddingTop: "2rem" }}>
-        {/* {pharmacistListItems} */}
-      </Grid>
+  useEffect(() => {
+    fetchData();
+  }, [filterType, selectedYear, selectedMonth, selectedMonthYear, selectedMedicine]);
 
-      <Grid item xs={12} sm={9} md={10} lg={10} xl={10} style={{ paddingLeft: "2rem" , marginTop:"0.5rem" }}>
-        <Typography variant="h3" gutterBottom style={{ marginLeft: "-0.8rem"}}>
+  return (
+    <Grid container>
+      <AdminHeader />
+
+      <Grid item xs={12} sm={9} md={10} lg={10} xl={10} style={{ paddingLeft: "20rem" }}>
+        <Typography variant="h4" gutterBottom>
           Sales
         </Typography>
 
-        <FormControl component="fieldset" style={{ marginBottom: "1rem" , marginTop:"-1rem"}}>
-  <FormLabel component="legend">Filter By:</FormLabel>
-  <RadioGroup row aria-label="filter-type" name="filter-type" value={filterType} onChange={handleFilterTypeChange}>
-    <FormControlLabel value="all" control={<Radio />} label="All" />
-    <FormControlLabel value="month" control={<Radio />} label="Month" />
-    {filterType === "month" && (
-      <div>
-        <InputLabel htmlFor="month-year">Month and Year</InputLabel>
-        <Select
-  value={`${selectedYear}-${selectedMonth}`}
-  onChange={handleMonthChange}
-  input={<Input id="month-year" />}
->
-  {Array.from({ length: new Date().getFullYear() - 1999 }, (_, yearIndex) => {
-    const year = 2000 + yearIndex;
-    return Array.from({ length: 12 }, (_, monthIndex) => {
-      const month = monthIndex + 1;
-      const formattedMonth = month < 10 ? `0${month}` : `${month}`;
-      return (
-        <MenuItem key={`${year}-${formattedMonth}`} value={`${year}-${formattedMonth}`}>
-          {`${formattedMonth}/${year}`}
-        </MenuItem>
-      );
-    });
-  })}
-</Select>
-
-      </div>
-    )}
-            <FormControlLabel value="medicine" control={<Radio />} label="Medicine" />
-            {filterType === "medicine" && (
-              <Select value={selectedMedicine} onChange={handleMedicineChange}>
-                {medicineNames.map((medicineName) => (
-                  <MenuItem key={medicineName} value={medicineName}>
-                    {medicineName}
-                  </MenuItem>
-                ))}
-              </Select>
-            )}
-          </RadioGroup>
+        <FormControl style={{ marginBottom: "1rem" }}>
+          <InputLabel htmlFor="filter-type">Filter By:</InputLabel>
+          <Select
+            id="filter-type"
+            value={filterType}
+            onChange={(e) => setFilterType(e.target.value)}
+            style={{ width: 150, fontSize: 14 }}
+          >
+            <MenuItem value="all">All</MenuItem>
+            <MenuItem value="month">Month</MenuItem>
+            <MenuItem value="medicine">Medicine</MenuItem>
+          </Select>
         </FormControl>
 
-        <Button variant="contained" onClick={handleShowSales} style = {{marginLeft:"1rem", marginTop : "0.5rem"}}> 
-          Show Sales
-        </Button>
+        {filterType === "month" && (
+          <FormControl style={{ marginBottom: "1rem" }}>
+            <InputLabel htmlFor="month-year">Month and Year:</InputLabel>
+            <Select
+              id="month-year"
+              value={selectedMonthYear}
+              onChange={handleMonthYearChange}
+              style={{ minWidth: 150 }}
+            >
+              {Array.from({ length: new Date().getFullYear() - 1999 }, (_, yearIndex) => {
+                const year = 2000 + yearIndex;
+                return Array.from({ length: 12 }, (_, monthIndex) => {
+                  const month = monthIndex + 1;
+                  const formattedMonth = month < 10 ? `0${month}` : `${month}`;
+                  return (
+                    <MenuItem key={`${year}-${formattedMonth}`} value={`${year}-${formattedMonth}`}>
+                      {`${formattedMonth}/${year}`}
+                    </MenuItem>
+                  );
+                });
+              })}
+            </Select>
+          </FormControl>
+        )}
+
+        {filterType === "medicine" && (
+          <FormControl style={{ marginBottom: "1rem" }}>
+            <InputLabel htmlFor="medicine">Medicine Name:</InputLabel>
+            <Select
+              id="medicine"
+              value={selectedMedicine}
+              onChange={handleMedicineChange}
+              style={{ minWidth: 150 }}
+            >
+              {medicineNames.map((medicineName) => (
+                <MenuItem key={medicineName} value={medicineName}>
+                  {medicineName}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        )}
 
         {isPending && <div>Loading...</div>}
         {error && <div>{error}</div>}
         {sales &&
-  sales.map((sale) => (
-  
-    <Paper key={sale.orderId} style={{ padding: "1rem", marginBottom: "1rem" }}>
-      <Typography variant="h6">Sale ID: {sale.orderId}</Typography>
-      <Typography>Date of Delivery: {new Date(sale.dateOfDelivery).toLocaleDateString()}</Typography>
-      <Typography>Total Price: {sale.totalPrice}</Typography>
-      <Typography variant="subtitle1">Order Details:</Typography>
-      {sale.orderDetails && sale.orderDetails.length > 0 ? (
-        sale.orderDetails.map((item, index) => (
-          <div key={index}>
-            <Typography>Medicine Name: {item.medicineName}</Typography>
-            <Typography>Quantity: {item.quantity}</Typography>
-          </div>
-        ))
-      ) : (
-        <Typography>No order details available.</Typography>
-      )}
-      
-    </Paper>
-  ))}
-<Paper>
-<Typography variant="h5">Total Sales: {calculateTotalPrice()}</Typography>
-</Paper>
-
-{/* <TableContainer component={Paper}></TableContainer>*/}
-
+          sales.map((sale) => (
+            <Paper key={sale.orderId} style={{ padding: "1rem", marginBottom: "1rem" }}>
+              <Typography variant="h6">Sale ID: {sale.orderId}</Typography>
+              <Typography>Date of Delivery: {new Date(sale.dateOfDelivery).toLocaleDateString()}</Typography>
+              <Typography>Total Price: {sale.totalPrice}</Typography>
+              <Typography variant="subtitle1">Order Details:</Typography>
+              {sale.orderDetails && sale.orderDetails.length > 0 ? (
+                sale.orderDetails.map((item, index) => (
+                  <div key={index}>
+                    <Typography>Medicine Name: {item.medicineName}</Typography>
+                    <Typography>Quantity: {item.quantity}</Typography>
+                  </div>
+                ))
+              ) : (
+                <Typography>No order details available.</Typography>
+              )}
+            </Paper>
+          ))}
+        <Paper>
+          <Typography variant="h5">Total Sales: {calculateTotalPrice()}</Typography>
+        </Paper>
       </Grid>
     </Grid>
-    </PharmacistHeader>
   );
 };
 
